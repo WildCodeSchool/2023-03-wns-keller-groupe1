3df14a7e4d1afba118e8b52a09a91dbdbc9a16d4
pok <yest@gmail.com>
2023-06-08 18:37:15 +0200

    TestConnexionOK

--- front/src/navigation/Navbar.tsx
+++ front/src/navigation/Navbar.tsx
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import leaf from "../assets/images/LeafWitheRbg.png";
 import logOut from "../assets/icons/logOut.svg";
-import home from "../assets/icons/home.svg";
-import chart from "../assets/icons/chart.svg";
-import social from "../assets/icons/social.svg";
-import puzzle from "../assets/icons/puzzle.svg";
-import progil from "../assets/icons/profil.svg";
+import { ReactComponent as DashboardIcon } from "../assets/icons/dashboard.svg";
+import { ReactComponent as ChartIcon } from "../assets/icons/chart.svg";
+import { ReactComponent as SocialIcon } from "../assets/icons/social.svg";
+import { ReactComponent as PuzzleIcon } from "../assets/icons/puzzle.svg";
+import { ReactComponent as ProfilIcon } from "../assets/icons/profil.svg";
 
 const Navbar = () => {
+  const [selectedIcon, setSelectedIcon] = useState<string | null>("dashboard");
+  const navigate = useNavigate();
+
+  const handleIconClick = (iconName: string) => {
+    setSelectedIcon(iconName);
+    navigate(`/${iconName}`);
+  };
+
   return (
     <div
       style={{
@@ -15,37 +25,107 @@ const Navbar = () => {
         backgroundColor: "#25A55F",
       }}
     >
-      <div style={{ height: "20%", display: "flex", justifyContent: "center" ,alignItems:'center', backgroundColor:'pink' }}>
-        <img src={leaf} alt="checkRegister" style={{ width: '60%', height: '70%' }} />
-      </div>
-      <div>
-        <img src={home} alt="checkRegister" style={{ width: 50, height: 50 }} />
+      <div
+        style={{
+          width: "100%",
+          height: "20%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <img
-          src={chart}
+          src={leaf}
           alt="checkRegister"
-          style={{ width: 50, height: 50 }}
-        />
-        <img
-          src={social}
-          alt="checkRegister"
-          style={{ width: 50, height: 50 }}
-        />
-        <img
-          src={puzzle}
-          alt="checkRegister"
-          style={{ width: 50, height: 50 }}
-        />
-        <img
-          src={progil}
-          alt="checkRegister"
-          style={{ width: 50, height: 50 }}
+          style={{ width: "60%", height: "70%", fill: "red" }}
         />
       </div>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "60%",
+          justifyContent: "center",
+          alignItems: "space-around",
+        }}
+      >
+        <DashboardIcon
+          className={`icon ${selectedIcon === "dashboard" ? "selected" : ""}`}
+          style={{
+            fill: selectedIcon === "dashboard" ? "white" : "#B5C2C9",
+            width: "30%",
+            height: "50%",
+            margin: "auto",
+            cursor: "pointer",          }}
+          onClick={() => handleIconClick("dashboard")}
+        />
+
+        <ChartIcon
+          className={`icon ${selectedIcon === "chart" ? "selected" : ""}`}
+          style={{
+            fill: selectedIcon === "chart" ? "white" : "#B5C2C9",
+            width: "30%",
+            height: "50%",
+            margin: "auto",
+            cursor: "pointer",
+          }}
+          onClick={() => handleIconClick("chart")}
+        />
+
+        <SocialIcon
+          className={`icon ${selectedIcon === "social" ? "selected" : ""}`}
+          style={{
+            fill: selectedIcon === "social" ? "white" : "#B5C2C9",
+            width: "30%",
+            height: "50%",
+            margin: "auto",
+            cursor: "pointer",
+          }}
+          onClick={() => handleIconClick("social")}
+        />
+
+        <PuzzleIcon
+          className={`icon ${selectedIcon === "puzzle" ? "selected" : ""}`}
+          style={{
+            fill: selectedIcon === "puzzle" ? "white" : "#B5C2C9",
+            width: "30%",
+            height: "50%",
+            margin: "auto",
+            cursor: "pointer",
+          }}
+          onClick={() => handleIconClick("puzzle")}
+        />
+
+        <ProfilIcon
+          className={`icon ${selectedIcon === "profil" ? "selected" : ""}`}
+          style={{
+            fill: selectedIcon === "profil" ? "white" : "#B5C2C9",
+            width: "30%",
+            height: "50%",
+            margin: "auto",
+            cursor: "pointer",
+          }}
+          onClick={() => handleIconClick("profil")}
+        />
+      </div>
+      <div
+        style={{
+          height: "20%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <img
           src={logOut}
           alt="checkRegister"
-          style={{ width: 50, height: 50 }}
+          style={{
+            width: "40%",
+            height: "50%",
+            cursor: "pointer",
+          }}
+          onClick={() => console.log("ok")}
         />
       </div>
     </div>
