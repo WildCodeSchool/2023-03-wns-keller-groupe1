6f9f1e61fdea8b85343a84e5e5532c91d8430741
pok <yest@gmail.com>
2023-06-08 16:01:23 +0200

    AppcontainerSetup2

--- front/src/navigation/Navbar.tsx
+++ front/src/navigation/Navbar.tsx
@@ -1,7 +1,25 @@
+import leaf from "../assets/images/LeafWitheRbg.png";
+import logOut from "../assets/icons/logOut.svg";
 const Navbar = () => {
-  return <div style={{width:200 , backgroundColor:'#FOF'}}>
-    pll
-  </div>;
+  return (
+    <div style={{ width: "10vw", height: "100vh", backgroundColor: "#25A55F" }}>
+      <div>
+        <img
+          src={leaf}
+          alt="checkRegister"
+          style={{ width: 150, height: 150 }}
+        />
+      </div>
+      <div></div>
+      <div>
+        <img
+          src={logOut}
+          alt="checkRegister"
+          style={{ width: 150, height: 150 }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
