dda043704cb191f77ff363c2075b0de7c724f398
pok <yest@gmail.com>
2023-07-12 10:28:21 +0200

    NavBarOk+TokenLogic+Tuto

--- app/Wild-carbon/index.tsx
+++ app/Wild-carbon/index.tsx
@@ -5,6 +5,7 @@ import Toast from 'react-native-toast-message';
 
 import App from "./src/navigation/appContainer";
 
+// il faut votre  Adresse IPv4 si vous utiliser pas un emulateur
 const client = new ApolloClient({
   uri: 'http://192.168.4.161:4000/', 
   cache: new InMemoryCache()
